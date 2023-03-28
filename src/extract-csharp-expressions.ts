c2396f534174820e89057d870c361a257c37712d
Ira Rainey <irarainey@microsoft.com>
2023-03-28 09:59:45 +0100

    Converted all file actions to synchronous.

--- src/extract-csharp-expressions.ts
+++ src/extract-csharp-expressions.ts
@@ -22,52 +22,21 @@ export const extract = (path: string, file: string) => {
 	const output = `${path.replace("policies/", "")}scripts/${file.replace(".xml", "")}`;
 
 	// Create the output directory
-	fs.mkdir(output, { recursive: true }, (err) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-	});
-	
+	fs.mkdirSync(output, { recursive: true });
+
 	// Copy the context class into the output directory
-	fs.copyFile(`${process.cwd()}/src/templates/context.csx`, `${output}/context.csx`, (err) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-	});
+	fs.copyFileSync(`${process.cwd()}/src/templates/context.csx`, `${output}/context.csx`);
 
 	// Copy the context settings into the output directory
-	fs.copyFile(`${process.cwd()}/src/templates/context.json`, `${output}/context.json`, (err) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-	});
+	fs.copyFileSync(`${process.cwd()}/src/templates/context.json`, `${output}/context.json`);
 
 	// Write the block snippets out as C# script files
 	blocks.forEach((match, index) => {
-		fs.writeFile(
-			`${output}/block-${(index + 1).toString().padStart(3, "0")}.csx`,
-			template.replace("return \"{0}\";", match),
-			(err) => {
-				if (err) {
-					console.error(err);
-				}
-			}
-		);
+		fs.writeFileSync(`${output}/block-${(index + 1).toString().padStart(3, "0")}.csx`, template.replace('return "{0}";', match));
 	});
 
 	// Write the inline snippets out as C# script files
 	inline.forEach((match, index) => {
-		fs.writeFile(
-			`${output}/inline-${(index + 1).toString().padStart(3, "0")}.csx`,
-			template.replace("\"{0}\"", match),
-			(err) => {
-				if (err) {
-					console.error(err);
-				}
-			}
-		);
+		fs.writeFileSync(`${output}/inline-${(index + 1).toString().padStart(3, "0")}.csx`, template.replace('"{0}"', match));
 	});
 };
